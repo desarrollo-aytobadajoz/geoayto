a1c8bcb100ba7a4ac7d4be7efef85cbad44c599a
jagudo <jagudo@aytobadajoz.es>
2023-08-07 19:34:01 +0200

    modified:   android/app/src/main/AndroidManifest.xml 	modified:   android/variables.gradle 	modified:   src/app/home/home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+//7 Importo el plugin geolocation
+import { Geolocation } from '@capacitor/geolocation';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +10,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public coords : any;
+
+  public latitud: number = 0;
+  public longitud: number = 0;
+  public precision: number = 0;
+  public altitud: number = 0;
+  public altitudPrecision: number = 0;
+  public velocidad: number = 0;
+  public cabecera: number = 0;
+
+  constructor() {
+
+    this.tomaUbicacion();
+
+  }
+
+  /*7 const printCurrentPosition =  7*/
+  async tomaUbicacion() /*7 => 7*/ {
+    const coordinates = await Geolocation.getCurrentPosition();
+  
+    //7 console.log('Current position:', coordinates);
+
+    this.coords = coordinates.coords;
+    this.latitud = coordinates.coords.latitude;
+    this.longitud = coordinates.coords.longitude;
+    this.precision = coordinates.coords.accuracy;
+    if (coordinates.coords.altitude) this.altitud = coordinates.coords.altitude;
+    if (coordinates.coords.altitudeAccuracy) this.altitudPrecision = coordinates.coords.altitudeAccuracy;
+    if (coordinates.coords.speed) this.velocidad = coordinates.coords.speed;
+    if (coordinates.coords.heading) this.cabecera =coordinates.coords.heading;
+  };
 
 }
