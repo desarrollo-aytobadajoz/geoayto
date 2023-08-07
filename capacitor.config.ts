0db2b16816e460103638fd7a14702719434cb436
jagudo <jagudo@aytobadajoz.es>
2023-08-07 18:45:57 +0200

    new file:   capacitor.config.ts 	modified:   ionic.config.json 	modified:   package-lock.json 	modified:   package.json

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.geoayto',
+  appName: 'geoayto',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
